9017cb0697ccc9a8246dd46e9b40ed274961cbc3
reamplify <reamplify@ya.ru>
2026-09-18 23:42:18 +0500

    feat: implement site header for unauthenticated user

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import './styles.scss';
 import { initRouter, registerRoute } from './app/router';
 import { renderHomePage } from './pages/home/home-page';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
 
 registerRoute('/', renderHomePage);
 
--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,7 +1,9 @@
+import { createHeader } from '../../components/header/header';
+
 export function renderHomePage(): void {
   const root = document.createElement('div');
   root.id = 'app';
-  root.textContent = 'Home Page (SPA placeholder)';
+  root.append(createHeader());
 
   document.body.replaceChildren(root);
 }
